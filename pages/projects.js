37ce0fbbf6176c23c4bea5df17587a07f81ab55c
Developer-Membrano <job.kennymembrano@gmail.com>
2022-09-14 12:23:38 +0800

    adding transitions in whole elements

--- pages/projects.js
+++ pages/projects.js
@@ -6,57 +6,87 @@ import { motion } from "framer-motion";
 const projects = () => {
     return(
         <>
-        <NavigationBar />
-            <div className={Projects.Heading}>
+            <NavigationBar />
+                <div className={Projects.Heading}>
+                    <motion.div
+                        initial="hidden" animate="visible" variants={{
+                            hidden:{
+                                // scale: .8,
+                                x: -100,
+                                opacity: 0,
+                            },
+                            visible: {
+                                // scale: 1,
+                                x: 0,
+                                opacity: 1,
+                                transition: {
+                                    delay: 1
+                                    
+                                }
+                            },
+                        }}>
+                        <h1>POSTERS</h1>
+                    </motion.div>
+                    
+                    <motion.div
+                        initial="hidden" animate="visible" variants={{
+                            hidden:{
+                                x: -100,
+                                opacity: 0,
+                            },
+                            visible: {
+                                x: 0,
+                                opacity: 1,
+                                transition: {
+                                    delay: 1   
+                                }
+                            },
+                        }}                    
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className= "w-20 h-20">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l-6 6m0 0l-6-6m6 6V9a6 6 0 0112 0v3" />
+                        </svg>
+                    </motion.div>
 
-                <motion.div
-                    initial="hidden" animate="visible" variants={{
-                        hidden:{
-                            // scale: .8,
-                            x: -100,
-                            opacity: 0,
-                        },
-                        visible: {
-                            // scale: 1,
-                            x: 0,
-                            opacity: 1,
-                            transition: {
-                                delay: .5
-                                
-                            }
-                        },
-                    }}>
-                    <h1>POSTERS</h1>
-                </motion.div>
-                
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className= "w-20 h-20">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l-6 6m0 0l-6-6m6 6V9a6 6 0 0112 0v3" />
-                    </svg>
-                
-
-            </div>
-
-            <section>
-                <div className= {Projects.parent}>
-                    <div className={Projects.div1}>
-                        <img src={"/img/Main-Poster_Audition.png"} />
-                    </div>
-                    <div className={Projects.div2}> 
-                        <img src={"/img/Feb_Event_poster.png"} />
-                    </div>
-                    <div className={Projects.div3}> 
-                        <img src={"/img/Solo_Group_Audition.png"} />
-                    </div>
-                    <div className={Projects.div4}> 
-                        <img src={"/img/womens_month.jpg"} />
-                    </div>
-                    <div className={Projects.div5}> 
-                        <img src={"/img/women_empowerment.png"} />
-                        <img src={"/img/stream_background.png"} />        
-                    </div>
                 </div>
-            </section>
-           
+            <motion.div
+                initial="hidden" animate="visible" variants={{
+                    hidden:{
+                    x: -100,
+                    opacity: 0,
+                    },
+                    visible: {
+                    x: 0,
+                    opacity: 1,
+                    transition: {
+                    delay: 1.2
+                                    
+                        }
+                    },
+                }}            
+            
+            >            
+                <section>
+                    <div className= {Projects.parent}>
+                        <div className={Projects.div1}>
+                            <img src={"/img/Main-Poster_Audition.png"} />
+                        </div>
+                        <div className={Projects.div2}> 
+                            <img src={"/img/Feb_Event_poster.png"} />
+                        </div>
+                        <div className={Projects.div3}> 
+                            <img src={"/img/Solo_Group_Audition.png"} />
+                        </div>
+                        <div className={Projects.div4}> 
+                            <img src={"/img/womens_month.jpg"} />
+                        </div>
+                        <div className={Projects.div5}> 
+                            <img src={"/img/women_empowerment.png"} />
+                            <img src={"/img/stream_background.png"} />        
+                        </div>
+                    </div>
+                </section>
+            </motion.div>
         </>
     )
 }
